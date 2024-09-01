ebb5eaedecd851d1628cd22070b857939d9f781b
Buravit Yenjit <khiaokrub@gmail.com>
2024-09-01 19:46:26 +0700

    Delete unnecessary line in models and views

--- polls/models.py
+++ polls/models.py
@@ -1,41 +1,5 @@
 import datetime
-
-from django.db import models
-from django.utils import timezone
 from django.contrib import admin
-
-
-# class Question(models.Model):
-#     """
-#     Represents a poll question.
-#
-#     Attributes:
-#         question_text (str): The text of the question.
-#         pub_date (datetime): The date and time the question was published.
-#     """
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField("date published")
-#
-#     def __str__(self):
-#         return self.question_text
-#
-#     @admin.display(
-#         boolean=True,
-#         ordering="pub_date",
-#         description="Published recently?",
-#     )
-#     def was_published_recently(self):
-#         """
-#         Checks if the question was published within the last day.
-#
-#         Returns:
-#             bool: True if the question was published within the last day,
-#                   False otherwise.
-#         """
-#         now = timezone.now()
-#         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
-
 from django.db import models
 from django.utils import timezone
 from datetime import timedelta
