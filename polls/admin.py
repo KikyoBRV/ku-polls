b419b1d220476af9428946ca407f5db13ddd0ebb
Buravit Yenjit <khiaokrub@gmail.com>
2024-09-08 22:01:30 +0700

    Add Vote to admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Choice, Question
+from .models import Choice, Question, Vote
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -17,3 +17,4 @@ class QuestionAdmin(admin.ModelAdmin):
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+admin.site.register(Vote)  # Register the Vote model
